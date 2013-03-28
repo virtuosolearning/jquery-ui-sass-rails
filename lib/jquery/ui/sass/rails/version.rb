6da68c0b45305365ff410b0b67c00d9aa204f817
Jakob Hilden <jakobhilden@gmail.com>
2013-03-28 11:07:27 +0100

    fix dependency

--- lib/jquery/ui/sass/rails/version.rb
+++ lib/jquery/ui/sass/rails/version.rb
@@ -2,7 +2,7 @@ module Jquery
   module Ui
     module Sass
       module Rails
-        VERSION = "4.0.2.beta1"
+        VERSION = "4.0.2.beta2"
         JQUERY_UI_RAILS_VERSION = "4.0.2"
       end
     end
