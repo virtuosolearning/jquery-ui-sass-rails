9b08d85dcda995229bed1a64c4569567f7d81268
Claudio Moratti <claudio@leevee.it>
2024-02-12 10:45:51 +0100

    Update version.rb

--- lib/jquery/ui/sass/rails/version.rb
+++ lib/jquery/ui/sass/rails/version.rb
@@ -3,7 +3,7 @@ module Jquery
     module Sass
       module Rails
         VERSION = "4.0.3.0"
-        JQUERY_UI_RAILS_VERSION = "4.0.3"
+        JQUERY_UI_RAILS_VERSION = "7.0.0"
       end
     end
   end
